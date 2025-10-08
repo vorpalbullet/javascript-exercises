6f92939bc8483eea245639529065141acf9c32f2
Jonathon Pearce <ghenghiz.cohen@gmail.com>
2025-10-08 23:37:54 +1000

    Solve 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (a < 0 || b < 0 || !(Number.isInteger(a) && Number.isInteger(b))) {
+    return "ERROR";
+  }
 
+  let end = Math.max(a, b);
+  let sum = 0;
+
+  for (let i = Math.min(a, b); i <= end; i++) sum += i;
+
+  return sum;
 };
 
 // Do not edit below this line
