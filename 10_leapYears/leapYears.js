dca08488ff4cb7a552942f00d8796299f3da7a7a
Jonathon Pearce <ghenghiz.cohen@gmail.com>
2025-10-08 23:52:02 +1000

    Solve 10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  // Leap years are years divisible by four (like 1984 and 2004).
+  // However, years divisible by 100 are not leap years (such as 1800 and 1900)
+  //  unless they are divisible by 400 (like 1600 and 2000, which were in fact
+  //  leap years). (Yes, it's all pretty confusing)
 
+  // return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+  if (year % 4 != 0) return false;
+  if (year % 100 != 0) return true;
+  return year % 400 == 0;
 };
 
 // Do not edit below this line
