046326906648159d85f6aaf04b021fab1701643f
Jonathon Pearce <ghenghiz.cohen@gmail.com>
2025-10-09 15:04:47 +1000

    Solve 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+// take two numbers and return their sum
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+// take two numbers and return the result of subtracting the second from the first
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+// take an array of numbers and add all the numbers together
+const sum = function (numbers) {
+  return numbers.reduce((sum, number) => (sum + number), 0);
 };
 
-const multiply = function() {
-
+// take an array of numbers and multiply all the numbers together
+const multiply = function (numbers) {
+  if (numbers.length == 0) return 0;
+  return numbers.reduce((product, number) => (product * number), 1);
 };
 
-const power = function() {
-	
+// take two numbers (base and exponent) and return the result of raising the base to that exponent
+const power = function (base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+// given a single number, find its [factorial](https://en.wikipedia.org/wiki/Factorial)
+const factorial = function (a) {
+  let result = 1;
+
+  for (let i = 2; i <= a; i++) result *= i;
+
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +40,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
