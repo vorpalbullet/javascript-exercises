22d070b617519c10493ea1931621ccdb47161432
Jonathon Pearce <ghenghiz.cohen@gmail.com>
2025-10-10 00:45:55 +1000

    Solve 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let arr = string
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "")
+    .split("");
 
+  return arr.join("") == arr.reverse().join("");
 };
 
 // Do not edit below this line
