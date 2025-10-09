bc642fbe759b9fa30c1e1706b37e3596e85bfa5d
Jonathon Pearce <ghenghiz.cohen@gmail.com>
2025-10-10 01:01:26 +1000

    Solve 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) return "OOPS";
 
+  const sqrtFive = Math.sqrt(5);
+
+  // use Binet's Formula
+  return Math.floor(
+    (1 / sqrtFive) * (((1 + sqrtFive) / 2) ** n - ((1 - sqrtFive) / 2) ** n)
+  );
 };
 
 // Do not edit below this line
