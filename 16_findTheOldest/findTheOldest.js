370c7c2764a9ea2d4ca9294cd92d9ddedac2fb30
Jonathon Pearce <ghenghiz.cohen@gmail.com>
2025-10-10 23:38:36 +1000

    Solve 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldest, current) => {
+    const oldestAge =
+      (oldest.yearOfDeath ?? new Date().getFullYear()) - oldest.yearOfBirth;
+    const currentAge =
+      (current.yearOfDeath ?? new Date().getFullYear()) - current.yearOfBirth;
 
+    return oldestAge < currentAge ? current : oldest;
+  }, people[0]);
 };
 
 // Do not edit below this line
