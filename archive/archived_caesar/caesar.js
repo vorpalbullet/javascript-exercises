4388d2b6a36e4796bdd851229d8af36b1ef2a10c
Jonathon Pearce <ghenghiz.cohen@gmail.com>
2025-10-11 09:37:32 +1000

    Solve archived_caesar

--- archive/archived_caesar/caesar.js
+++ archive/archived_caesar/caesar.js
@@ -1,5 +1,22 @@
-const caesar = function() {
+const caesar = function (string, shift) {
+  const lowerAlphas = "abcdefghijklmnopqrstuvwxyz";
+  const upperAlphas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+  return string
+    .split("")
+    .map((char) => {
+      let index = lowerAlphas.indexOf(char);
+      let isLower = index != -1;
+      if (!isLower) index = upperAlphas.indexOf(char);
+      if (index == -1) return char;
+
+      let newIndex = index + shift;
+      while (newIndex < 0) newIndex += 26;
+      while (newIndex > 25) newIndex -= 26;
+
+      return (isLower ? lowerAlphas : upperAlphas).at(newIndex);
+    })
+    .join("");
 };
 
 // Do not edit below this line
