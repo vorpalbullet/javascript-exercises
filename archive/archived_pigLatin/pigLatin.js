d06f865fa5bcc6c838f3e149082b8f57b89f5997
Jonathon Pearce <ghenghiz.cohen@gmail.com>
2025-10-11 23:21:37 +1000

    Solve archived_pigLatin

--- archive/archived_pigLatin/pigLatin.js
+++ archive/archived_pigLatin/pigLatin.js
@@ -1,6 +1,12 @@
 function pigLatin(string) {
+  // While this passes all the exercise tests, it doesn't properly handle
+  // words without vowels (cry, by, gym, lynx, myth).
 
-};
-  
+  // Although, it handles them better than the suggested solution, which throws
+  // an unhandled TypeError
+  let regexp = /\b((?:[B-DF-HJ-NP-TV-Z]*(?:(?<=q)u)?)*)([A-Z]+)/gi;
+  return string.replace(regexp, '$2$1ay');
+}
+
 // Do not edit below this line
 module.exports = pigLatin;
